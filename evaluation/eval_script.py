ad7629ab7cfd5778161a6ebb14ee73354c183d53
Boris Zimka <zimkab@qavm-f9b691f5.qemu>
2025-10-05 20:20:34 +0300

    Speed up metrics, add try-catch

--- evaluation/eval_script.py
+++ evaluation/eval_script.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from pathlib import Path
 import pprint
 import time
@@ -9,58 +8,61 @@ from worldscore.benchmark.metrics import CLIPScoreMetric, CLIPImageQualityAssess
     CameraErrorMetric, OpticalFlowAverageEndPointErrorMetric,\
     ObjectDetectionMetric, ReprojectionErrorMetric, OpticalFlowMetric
 
+RAISE_FAILS = False
+
 
 class MlCupMetricScorer:
     SKIP_ASPECTS = ['motion_accuracy', 'motion_smoothness', 'motion_magnitude']
     SKIP_METRICS = [
         'camera_error', # TODO: camera intrinsics
-        'reprojection_error', # TODO: CUDA error
         'object_detection', # interface: no prompt
         'clip_score' # interface: no prompt
+        #'reprojection_error', works but useless
     ]
 
-    METRIC_CLASSES = {
-        "clip_iqa+": CLIPImageQualityAssessmentPlusMetric,
-        "clip_aesthetic": IQACLIPAestheticScoreMetric,
-        "gram_matrix": GramMatrixMetric,
-        "optical_flow_aepe": OpticalFlowAverageEndPointErrorMetric,
-        "camera_error": CameraErrorMetric,
-        "object_detection": ObjectDetectionMetric,
-        "reprojection_error": ReprojectionErrorMetric,
-        "optical_flow": OpticalFlowMetric,
-        "clip_score": CLIPScoreMetric,
-    }
-
     def __init__(self):
-        self._metrics = deepcopy(ASPECT_INFO)
+        self._scorers = {
+            "clip_iqa+": CLIPImageQualityAssessmentPlusMetric(),
+            "clip_aesthetic": IQACLIPAestheticScoreMetric(),
+            "gram_matrix": GramMatrixMetric(),
+            "optical_flow_aepe": OpticalFlowAverageEndPointErrorMetric(),
+            "optical_flow": OpticalFlowMetric(),
+            #"camera_error": CameraErrorMetric(),
+            #"object_detection": ObjectDetectionMetric(),
+            #"reprojection_error": ReprojectionErrorMetric(),
+            #"clip_score": CLIPScoreMetric(),
+        }
 
     def __call__(self, image_paths: list[str]):
         metrics_result = {}
-        aspects_scores = []
         t_init = time.time()
         for aspect, aspect_data in ASPECT_INFO.items():
             if aspect in self.SKIP_ASPECTS:
                 continue
             metrics_result[aspect] = {}
-            current_aspect_scores = []
             for metric_name, metric_data in aspect_data['metrics'].items():
                 if metric_name in self.SKIP_METRICS:
                     continue
-                t_start = time.time()
-                scorer_cls = self.METRIC_CLASSES[metric_name]
-                scorer = scorer_cls()
                 args = [image_paths]
                 if metric_name == "gram_matrix":
                     args = [image_paths[0], image_paths[1:]]
-                score_value = scorer._compute_aspect_scores(*args)
-                current_aspect_scores.append(score_value)
-                metrics_result[aspect][metric_name] = {'score' : score_value, 'time' : time.time() - t_start}
 
-            if len(current_aspect_scores) > 0:
-                aspects_scores.append(sum(current_aspect_scores) / (len(current_aspect_scores)))
-
+                t_start = time.time()
+                try:
+                    score_value = self._scorers[metric_name]._compute_scores(*args)
+                    metrics_result[aspect][metric_name] = {'score' : score_value, 'time' : time.time() - t_start}
+                except Exception as exc:
+                    if RAISE_FAILS:
+                        raise
         metrics_result = renormalize_score(metrics_result)
-        metrics_result['total_score'] = sum(aspects_scores) / (len(aspects_scores) + 1e-6)
+        aspect_normalized_scores = []
+        for aspect, aspect_scores in metrics_result.items():
+            if len(aspect_scores) > 0:
+                print(aspect_scores)
+                aspect_normalized_scores.append(
+                    sum(m['score_normalized'] for m in aspect_scores.values()) / len(aspect_scores)
+                )
+        metrics_result['avg_normalized_score'] = sum(aspect_normalized_scores) / len(aspect_normalized_scores)
         metrics_result['total_time'] = time.time() - t_init
         return metrics_result
 
@@ -68,11 +70,13 @@ class MlCupMetricScorer:
 if __name__ == "__main__":
     import sys
     assert len(sys.argv) > 1, sys.argv
+    t0 = time.time()
+    mlcup_scorer = MlCupMetricScorer()
     for folder_path in sys.argv[1:]:
         root = Path(folder_path).absolute()
         images = list([str(x.absolute()) for x in root.glob("*.png")])
-        mlcup_scorer = MlCupMetricScorer()
         result = mlcup_scorer(images)
         print("=========")
         print(folder_path)
         pprint.pprint(result, compact=True)
+    print(f"Total time: {time.time() - t0}")
